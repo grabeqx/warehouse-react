b82f4327519b444b8aad1e1be8957496bc057638
grabeqx <grabowski.kontakt@gmail.com>
2018-03-21 17:46:11 +0100

    First component

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ButtonAppBar from './containers/appBar';
+
+ReactDOM.render(<div>
+    <ButtonAppBar></ButtonAppBar>
+</div>, document.querySelector('#root'));
--- src/vendor.js
+++ src/vendor.js
@@ -1,2 +1,6 @@
+require('./styles/index.scss');
+
 import React from 'react';
 import reactDOM from 'react-dom';
+import axios from 'axios';
+import "regenerator-runtime/runtime";
