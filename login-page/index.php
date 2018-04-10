38085b37ba3f81df328b2e0f7999803801268322
grabeqx <grabowski.kontakt@gmail.com>
2018-04-10 19:06:30 +0200

    Fase fix 2

--- login-page/index.php
+++ login-page/index.php
@@ -20,7 +20,7 @@ if (isset($_POST['username']) and isset($_POST['password'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="text/html; charset=utf-8">
-    <link rel="stylesheet" href="styles/login.css">
+    <link rel="stylesheet" href="/styles/login.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&amp;subset=latin-ext" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="/dist/style.css">
